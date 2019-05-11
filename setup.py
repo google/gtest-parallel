62b17e97d0eccd3fee9fca711c03ef4c432bf071
kshithijiyer <kshithij.ki@gmail.com>
2019-05-11 17:26:02 +0530

    Adding setup.py, version.py and modifying project structure.
    Signed-off-by: kshithijiyer <kshithij.ki@gmail.com>

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# Copyright 2019 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from setuptools import find_packages, setup
+
+# This reads the __version__ variable from gtest-parallel/version.py
+with open('gtest-parallel/version.py') as f:
+  exec(f.read(), globals())
+
+description = ('Run Google Test suites in parallel.')
+
+# Reading long Description from README.md file.
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'gtest-parallel',
+    version = __version__,
+    url = 'http://github.com/google/gtest-parallel',
+    author = 'Google Inc.',
+    author_email = 'googletestframework@googlegroups.com',
+    license = 'Apache 2.0',
+    description = description,
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = find_packages(),
+    scripts=['gtest-parallel/gtest-parallel']
+)
+
